682f5ae4fcd1f1d02f5be07b71a320cd889c962f
linnlatt <linn.latt.yamone.qz@gmail.com>
2026-07-17 14:23:58 +0700

    show event card 7

--- compo_lab2/src/views/HomeView.vue
+++ compo_lab2/src/views/HomeView.vue
@@ -1,10 +1,9 @@
-<!-- <script setup lang="ts"> -->
-<!-- import TheWelcome from '../components/TheWelcome.vue' -->
-<!-- </script> -->
+<script setup lang="ts">
+import EventCard from '@/components/EventCard.vue'
+</script>
 
 <template>
-  <!-- <main>
-    <TheWelcome />
-  </main> -->
-  <div class="home"></div>
+  <div class="home">
+    <eventCard />
+  </div>
 </template>
