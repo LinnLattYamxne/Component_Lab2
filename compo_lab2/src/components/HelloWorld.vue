01dfe9bd508e50a1b0485abfad79d36a57ea590c
linnlatt <linn.latt.yamone.qz@gmail.com>
2026-07-17 13:43:29 +0700

    browser changes 3.2

--- compo_lab2/src/components/HelloWorld.vue
+++ compo_lab2/src/components/HelloWorld.vue
@@ -1,21 +1,21 @@
 <script setup lang="ts">
-defineProps<{
-  msg: string
-}>()
+// defineProps<{
+//   msg: string
+// }>()
 </script>
 
 <template>
   <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
+    <!-- <h1 class="green">{{ msg }}</h1>
     <h3>
       You’ve successfully created a project with
       <a href="https://vite.dev/" target="_blank" rel="noopener">Vite</a> +
       <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>. What's next?
-    </h3>
+    </h3> -->
   </div>
 </template>
 
-<style scoped>
+<!-- <style scoped>
 h1 {
   font-weight: 500;
   font-size: 2.6rem;
@@ -38,4 +38,4 @@ h3 {
     text-align: left;
   }
 }
-</style>
+</style> -->
