7965ccc9e8534cc256754a3b157efac597481a3b
linnlatt <linn.latt.yamone.qz@gmail.com>
2026-07-19 00:15:50 +0700

    refactor: fixed path (11)

--- compo_lab2/src/router/index.ts
+++ compo_lab2/src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import EventListView from '../views/EventListView.vue/index.js'
+import EventListView from '../views/EventListView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
