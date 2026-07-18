10fb76c2888f416df0d95c017145236792f45f2b
linnlatt <linn.latt.yamone.qz@gmail.com>
2026-07-19 00:14:25 +0700

    change name from HoneView.vue to EventListView.vue 10,11

--- compo_lab2/src/router/index.ts
+++ compo_lab2/src/router/index.ts
@@ -1,13 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import EventListView from '../views/EventListView.vue/index.js'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView,
+      name: 'event-view-list',
+      component: EventListView,
     },
     {
       path: '/about',
