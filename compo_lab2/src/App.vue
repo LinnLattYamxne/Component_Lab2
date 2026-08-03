12f3d2e7cca731f4a22acf8315e1afd1d1cdb8a6
linnlatt <linn.latt.yamone.qz@gmail.com>
2026-08-03 12:49:16 +0700

    Lab4 edit button flash message (1.6)

--- compo_lab2/src/App.vue
+++ compo_lab2/src/App.vue
@@ -1,13 +1,22 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
+import { useMessageStore } from '@/stores/message'
+import { storeToRefs } from 'pinia'
+
+const store = useMessageStore()
+const { message } = storeToRefs(store)
 </script>
 
 <template>
   <div id="app">
     <header>
+      <div id="flashMessage" v-if="message">
+        <h4>{{ message }}</h4>
+      </div>
       <div class="wrapper">
         <nav>
           <RouterLink :to="{ name: 'event-list-view' }">Event</RouterLink> |
+          
           <RouterLink :to="{ name: 'about' }">About</RouterLink>
         </nav>
       </div>
@@ -52,4 +61,17 @@ nav a {
  h2 {
   font-size: 20px
  }
+
+ @keyframes yellowFade {
+  from {
+    background-color: yellow;
+  }
+  to {
+    background-color: transparent;
+  }
+}
+
+#flashMessage {
+  animation: yellowFade 3s ease-in-out;
+}
 </style>
