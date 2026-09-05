d7ee3e38145ad55e7dbc4ff9adf0cfe608bb0f47
linnlatt <linn.latt.yamone.qz@gmail.com>
2026-09-06 01:03:03 +0700

    change backend server

--- compo_lab2/src/services/EventService.ts
+++ compo_lab2/src/services/EventService.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 
 const apiClient = axios.create({
-    baseURL: 'https://my-json-server.typicode.com/LinnLattYamxne/db.json_forLab2',
+    baseURL: 'http://localhost:8080',
     withCredentials: false,
     headers: {
         Accept: 'application/json',
