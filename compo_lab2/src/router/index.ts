8dad2696ff17e6a9854f4007dd2253f032706a69
linnlatt <linn.latt.yamone.qz@gmail.com>
2026-09-14 01:41:25 +0700

    Lab8 add EventForm (6.7)

--- compo_lab2/src/router/index.ts
+++ compo_lab2/src/router/index.ts
@@ -10,6 +10,7 @@ import NetworkErrorView from '@/views/NetworkErrorView.vue'
 import nProgress from 'nprogress'
 import EventService from '@/services/EventService'
 import { useEventStore } from '@/stores/event'
+import AddEventView from '@/views/event/EventFormView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -84,6 +85,11 @@ const router = createRouter({
       path: '/:catchAll(.*)',
       name: 'not-found',
       component: NotFoundView
+    },
+    {
+      path: '/add-event',
+      name: 'add-event',
+      component: AddEventView
     }
   ],
   scrollBehavior(to, from, savedPosition) {
---
+++ compo_lab2/src/views/event/EventFormView.vue
@@ -0,0 +1,67 @@
+<script setup lang="ts">
+import type { Event } from '@/types'
+import { ref } from 'vue'
+
+const event = ref<Event>({
+  id: null,
+  category: '',
+  title: '',
+  description: '',
+  location: '',
+  date: '',
+  time: '',
+  petsAllowed: false,
+  organizer: ''
+})
+</script>
+
+<template>
+  <div>
+    <h1>Create an event</h1>
+    <form>
+      <h3>Name & describe your event</h3>
+      <label class="block text-gray-500 font-bold">Category</label>
+      <input
+        v-model="event.category"
+        type="text"
+        placeholder="Category"
+        class="h-13 w-full px-2.5 text-xl border border-gray-400 focus:border-emerald-500 focus:outline-none mb-6"
+      />
+
+      <h3>Name & describe your event</h3>
+      <label class="block text-gray-500 font-bold">Title</label>
+      <input
+        v-model="event.title"
+        type="text"
+        placeholder="Title"
+        class="h-13 w-1/4 px-2.5 text-xl border border-gray-400 focus:border-emerald-500 focus:outline-none mb-6"
+      />
+
+      <label class="block text-gray-500 font-bold">Description</label>
+      <input
+        v-model="event.description"
+        type="text"
+        placeholder="Description"
+        class="h-13 w-1/4 px-2.5 text-xl border border-gray-400 focus:border-emerald-500 focus:outline-none mb-6"
+      />
+
+      <h3>Where is your event?</h3>
+      <label class="block text-gray-500 font-bold">Location</label>
+      <input
+        v-model="event.location"
+        type="text"
+        placeholder="Location"
+        class="h-13 w-1/4 px-2.5 text-xl border border-gray-400 focus:border-emerald-500 focus:outline-none mb-6"
+      />
+
+      <button
+        class="flex w-fit mx-auto items-center justify-center h-13 px-10 rounded-md font-semibold whitespace-nowrap border border-gray-400 focus:border-emerald-500 transition-all duration-200 ease-linear hover:scale-105 hover:border-emerald-500 hover:shadow-lg active:scale-100 focus:outline-none"
+        type="submit"
+      >
+        Submit
+      </button>
+    </form>
+
+    <pre>{{ event }}</pre>
+  </div>
+</template>
