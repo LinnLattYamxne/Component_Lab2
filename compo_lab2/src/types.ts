5271ab926818f92644bfef5bbe815f41b0c0c448
linnlatt <linn.latt.yamone.qz@gmail.com>
2026-09-14 14:28:05 +0700

    Lab9 add BaseInput (5.2)

--- compo_lab2/src/types.ts
+++ compo_lab2/src/types.ts
@@ -7,7 +7,12 @@ export interface Event {
     date: string
     time: string
     petsAllowed: boolean
-    organizer: string
+    organizer: Organizer
+}
+
+export interface Organizer {
+  id: number
+  name: string
 }
 
 export interface Student {
