7032a1fdd3f4224d935ab72e9b6caa5b7d1aeec5
linnlatt <linn.latt.yamone.qz@gmail.com>
2026-09-21 13:34:41 +0700

    Lab9 add BaseSelect component (6.6)

--- compo_lab2/src/views/event/EventFormView.vue
+++ compo_lab2/src/views/event/EventFormView.vue
@@ -6,6 +6,7 @@ import { useRouter } from 'vue-router'
 import { useMessageStore } from '@/stores/message'
 import BaseInput from '@/components/BaseInput.vue'
 import OrganizerService from '@/services/OrganizerService'
+import BaseSelect from '@/components/BaseSelect.vue'
 
 const event = ref<Event>({
   id: null,
@@ -32,6 +33,10 @@ function saveEvent() {
       setTimeout(() => {
         store.resetMessage()
       }, 3000)
+      router.push({
+        name: 'event-detail-view',
+        params: {id: response.data.id}
+      })
     })
     .catch(() => {
       router.push({ name: 'network-error-view' })
@@ -64,28 +69,11 @@ onMounted(() => {
       <h3>Where is your event?</h3>
       <BaseInput v-model="event.location" type="text" label="Location" />
 
-      <label class="block text-gray-500 font-bold">Select an Organizer</label>
-      <div class="relative w-1/4 mb-6 mx-auto">
-        <select
-          class="h-13 w-full pl-2.5 pr-6 text-xl align-middle appearance-none rounded-none border border-gray-400 focus:border-emerald-500 focus:outline-none"
-          v-model="event.organizer.id"
-        >
-          <option
-            v-for="option in organizers"
-            :value="option.id"
-            :key="option.id"
-            :selected="option.id === event.organizer.id"
-          >
-            {{ option.name }}
-          </option>
-        </select>
-        <svg
-          class="pointer-events-none absolute right-3 top-1/2 -translate-y-1/2 w-2 h-2.5 fill-gray-700"
-          viewBox="0 0 4 5"
-        >
-          <path d="M2 0L0 2h4zm0 5L0 3h4z" />
-        </svg>
-      </div>
+      <h3>Who is your organizer?</h3>
+      <label>Select an Organizer</label>
+
+      <BaseSelect v-model="event.organizer.id" :options="organizers" label="Organizer"/>
+      
       
       <button
         class="flex w-fit mx-auto items-center justify-center h-13 px-10 rounded-md font-semibold whitespace-nowrap border border-gray-400 focus:border-emerald-500 transition-all duration-200 ease-linear hover:scale-105 hover:border-emerald-500 hover:shadow-lg active:scale-100 focus:outline-none"
